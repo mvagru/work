f2398f00b1f736d9c26253a16218910a99418635
mvagru <mvagru@outlook.com>
2015-08-27 14:34:09 +0500

    0.0.1

---
+++ Work/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.моиЗадачиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заявкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.управлениеЗадачамиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инвентаризацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.архивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.панельАдминистратораToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.моиЗадачиToolStripMenuItem,
+            this.заявкиToolStripMenuItem,
+            this.управлениеЗадачамиToolStripMenuItem,
+            this.инвентаризацияToolStripMenuItem,
+            this.архивToolStripMenuItem,
+            this.панельАдминистратораToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 50);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // моиЗадачиToolStripMenuItem
+            // 
+            this.моиЗадачиToolStripMenuItem.AutoSize = false;
+            this.моиЗадачиToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.моиЗадачиToolStripMenuItem.Name = "моиЗадачиToolStripMenuItem";
+            this.моиЗадачиToolStripMenuItem.Size = new System.Drawing.Size(150, 50);
+            this.моиЗадачиToolStripMenuItem.Text = "Мои задачи";
+            // 
+            // заявкиToolStripMenuItem
+            // 
+            this.заявкиToolStripMenuItem.AutoSize = false;
+            this.заявкиToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.заявкиToolStripMenuItem.Name = "заявкиToolStripMenuItem";
+            this.заявкиToolStripMenuItem.Size = new System.Drawing.Size(150, 50);
+            this.заявкиToolStripMenuItem.Text = "Заявки";
+            // 
+            // управлениеЗадачамиToolStripMenuItem
+            // 
+            this.управлениеЗадачамиToolStripMenuItem.AutoSize = false;
+            this.управлениеЗадачамиToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.управлениеЗадачамиToolStripMenuItem.Name = "управлениеЗадачамиToolStripMenuItem";
+            this.управлениеЗадачамиToolStripMenuItem.Size = new System.Drawing.Size(150, 50);
+            this.управлениеЗадачамиToolStripMenuItem.Text = "Управление задачами";
+            this.управлениеЗадачамиToolStripMenuItem.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            // 
+            // инвентаризацияToolStripMenuItem
+            // 
+            this.инвентаризацияToolStripMenuItem.AutoSize = false;
+            this.инвентаризацияToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.инвентаризацияToolStripMenuItem.Name = "инвентаризацияToolStripMenuItem";
+            this.инвентаризацияToolStripMenuItem.Size = new System.Drawing.Size(150, 50);
+            this.инвентаризацияToolStripMenuItem.Text = "Инвентаризация";
+            // 
+            // архивToolStripMenuItem
+            // 
+            this.архивToolStripMenuItem.AutoSize = false;
+            this.архивToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.архивToolStripMenuItem.Name = "архивToolStripMenuItem";
+            this.архивToolStripMenuItem.Size = new System.Drawing.Size(150, 50);
+            this.архивToolStripMenuItem.Text = "Архив";
+            // 
+            // панельАдминистратораToolStripMenuItem
+            // 
+            this.панельАдминистратораToolStripMenuItem.AutoSize = false;
+            this.панельАдминистратораToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.панельАдминистратораToolStripMenuItem.Name = "панельАдминистратораToolStripMenuItem";
+            this.панельАдминистратораToolStripMenuItem.Size = new System.Drawing.Size(180, 50);
+            this.панельАдминистратораToolStripMenuItem.Text = "Панель администратора";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 50);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(150, 935);
+            this.toolStrip1.TabIndex = 1;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(148, 23);
+            this.toolStripButton1.Text = "Мои задачи";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(148, 23);
+            this.toolStripButton2.Text = "Услуги в работе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1264, 985);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ПК ОКС";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem моиЗадачиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заявкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem управлениеЗадачамиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem инвентаризацияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem архивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem панельАдминистратораToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+
+    }
+}
+
